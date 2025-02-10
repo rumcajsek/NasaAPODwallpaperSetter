7685dc0f6e158d92d38130de5b9535faa4b1a38e
Jakub Krol <jakub.krol@capgemini.com>
2025-02-10 08:41:17 +0100

    made script os-agnostic

--- apodScript.py
+++ apodScript.py
@@ -1,16 +1,19 @@
+from pathlib import Path
 import os
 import datetime
 import requests
-import ctypes
 import logging
 import json
-import winreg
-from pathlib import Path
+import platform
+if platform.system() == "Windows":
+    import winreg
+    import ctypes
+
 
 # Configuration
 NASA_API_KEY = "DEMO_KEY"  # Replace with your NASA API key
-SAVE_FOLDER_PATH = f"{Path(__file__).parent}\\NASA_APOD"
-JSON_STATE_FILE = f"{SAVE_FOLDER_PATH}\\state.json"
+SAVE_FOLDER_PATH = os.path.join(Path(__file__).parent, "NASA_APOD")
+JSON_STATE_FILE = os.path.join(SAVE_FOLDER_PATH,"state.json")
 APOD_URL = f"https://api.nasa.gov/planetary/apod"
 DATE_TODAY = datetime.datetime.now().strftime("%Y-%m-%d")
 STYLE_MAP = {
@@ -25,7 +28,7 @@ STYLE_MAP = {
 os.makedirs(SAVE_FOLDER_PATH, exist_ok=True)
 
 logging.basicConfig(
-    filename=f"{SAVE_FOLDER_PATH}\\apod_logs.log",
+    filename=os.path.join(SAVE_FOLDER_PATH,"apod_logs.log"),
     filemode="a",
     format="%(asctime)s [%(levelname)s] %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
@@ -124,27 +127,35 @@ def set_wallpaper(image_path, style="fill"):
     """
     Sets the provided image as the desktop wallpaper.
     """
+    system = platform.system()
     if style not in STYLE_MAP:
         logging.error(f"Invalid style '{style}' provided.")
         get_and_decrement_run_count()
         return None
     
-    wp_style, wp_tile = STYLE_MAP[style]
+    if system == "Windows":
+        wp_style, wp_tile = STYLE_MAP[style]
+        try:
+            if os.path.exists(image_path):
+                reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Control Panel\Desktop", 0, winreg.KEY_SET_VALUE)
+                winreg.SetValueEx(reg_key, "WallpaperStyle", 0, winreg.REG_SZ, str(wp_style))
+                winreg.SetValueEx(reg_key, "TileWallpaper", 0, winreg.REG_SZ, str(wp_tile))
+                winreg.CloseKey(reg_key)
 
-    try:
-        if os.path.exists(image_path):
-            reg_key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Control Panel\Desktop", 0, winreg.KEY_SET_VALUE)
-            winreg.SetValueEx(reg_key, "WallpaperStyle", 0, winreg.REG_SZ, str(wp_style))
-            winreg.SetValueEx(reg_key, "TileWallpaper", 0, winreg.REG_SZ, str(wp_tile))
-            winreg.CloseKey(reg_key)
-
-            ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
-            logging.info(f"Wallpaper set to: {image_path}, Wallpaper style: {style}")
-        else:
-            logging.error(f"Wallpaper file not found: {image_path}")
-    except Exception as e:
-        logging.error(f"Error setting registry values for wallpaper: {str(e)}")
-        get_and_decrement_run_count()
+                ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
+                logging.info(f"Wallpaper set to: {image_path}, Wallpaper style: {style}")
+            else:
+                logging.error(f"Wallpaper file not found: {image_path}")
+        except Exception as e:
+            logging.error(f"Error setting registry values for wallpaper: {str(e)}")
+            get_and_decrement_run_count()
+            return None
+    elif system == "Darwin": # MacOS, not tested!
+        os.system(f"osascript -e 'tell application \"Finder\" to set desktop picture to POSIX file \"{image_path}\"'")
+    elif system == "Linux": 
+        os.system(f"gsettings set org.gnome.desktop.background picture-uri file://{image_path}")
+    else:
+        logging.error("Unsupported OS for wallpaper setting.")
         return None
 
 if __name__ == "__main__":
